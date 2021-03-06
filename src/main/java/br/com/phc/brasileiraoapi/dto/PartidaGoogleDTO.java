eafa9a9f5156f426b44acd25fb36637736d28950
heitorserathiuk <heitormarfurte1@gmail.com>
2021-03-06 00:21:26 -0300

    Ajustes

--- src/main/java/br/com/phc/brasileiraoapi/dto/PartidaGoogleDTO.java
+++ src/main/java/br/com/phc/brasileiraoapi/dto/PartidaGoogleDTO.java
@@ -2,7 +2,9 @@ package br.com.phc.brasileiraoapi.dto;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
