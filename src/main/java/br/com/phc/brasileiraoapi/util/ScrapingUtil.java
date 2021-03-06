fc68942328a2f0524c8e5b7a17f4d368eb502c63
heitorserathiuk <heitormarfurte1@gmail.com>
2021-03-06 00:16:26 -0300

    Criacao do dto com dados de partida, criacao do enum de status, e outros ajustes

---
+++ src/main/java/br/com/phc/brasileiraoapi/util/ScrapingUtil.java
@@ -0,0 +1,14 @@
+package br.com.phc.brasileiraoapi.util;
+
+public class ScrapingUtil {
+
+	private static final String BASE_URL_GOOGLE = "https://www.google.com.br/search?q=";
+	private static final String COMPLEMENTO_URL_GOOGLE = "&hl=pt-BR";
+	
+	public static void main(String[] args) {
+
+		
+
+	}
+
+}
